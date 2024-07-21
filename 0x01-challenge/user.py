2e07a94bc8fcdfc9ba207441589f0db413d7c947
AbdelRahman Adel AbdelFatttah <56457765+ad3ldev@users.noreply.github.com>
2024-07-22 00:09:05 +0300

    different approach

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
-""" 
+"""
 User class
 """
 
+
 class User:
+    """Documentation"""
+
     def __init__(self):
+        """Documentation"""
         self.__email = None
 
     @property
     def email(self):
-        """Getter for email"""
+        """Documentation"""
         return self.__email
 
     @email.setter
     def email(self, value):
-        """Setter for email"""
-        if not isinstance(value, str):
+        """Documentation"""
+        if type(value) is not str:
             raise TypeError("email must be a string")
         self.__email = value
+
 
 if __name__ == "__main__":
     u = User()
